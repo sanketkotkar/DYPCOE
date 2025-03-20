4a286a9021beff026e3e544eb99c689828f7666a
Sanket Mohan Kotkar <sanketkotkar016@gmail.com>
2025-03-20 18:24:06 +0530

    Pr13.cpp

--- SECO2425C073 (4th Sem)/DSAL/Pr13.cpp
+++ SECO2425C073 (4th Sem)/DSAL/Pr13.cpp
@@ -4,59 +4,58 @@
 #include<queue>
 using namespace std;
 
-void displayAdjList(vector<vector<int>>& matrix, vector<char>& vname){
+// Function to display adjacency list
+void displayAdjList(vector<vector<int>>& adjList, vector<char>& vname){
     cout << "\nAdjacency List representation:\n";
     for (int i = 0; i < vname.size(); i++) {
         cout << vname[i] << " -> ";
-        for (int j = 0; j < matrix[i].size(); j++) {
-            if (matrix[i][j] != 0) {
-                cout << vname[j] << " (weight: " << matrix[i][j] << ") ";
-            }
+        for (int neighbor : adjList[i]) {
+            cout << vname[neighbor] << " ";
         }
         cout << endl;
     }
 }
 
-void BFSTraversal(vector<vector<int>>& matrix, vector<char>& vname){
-    int n = matrix.size();
+// BFS Traversal using adjacency list
+void BFSTraversal(vector<vector<int>>& adjList, vector<char>& vname){
+    int n = adjList.size();
     vector<bool> visited(n, false);
     queue<int> q;
-    
+
     cout << "\nBFS Traversal: ";
     q.push(0);
     visited[0] = true;
-    
+
     while(!q.empty()){
         int current = q.front();
         q.pop();
-        
         cout << vname[current] << " ";
-        
-        for(int i = 0; i < n; i++){
-            if(matrix[current][i] != 0 && !visited[i]){
-                q.push(i);
-                visited[i] = true;
+
+        for(int neighbor : adjList[current]){
+            if(!visited[neighbor]){
+                q.push(neighbor);
+                visited[neighbor] = true;
             }
         }
     }
     cout << endl;
 }
 
+// DFS Traversal using adjacency matrix
 void DFSTraversal(vector<vector<int>>& matrix, vector<char>& vname) {
     int n = matrix.size();
     vector<bool> visited(n, false);
     stack<int> s;
-    
+
     cout << "\nDFS Traversal: ";
     s.push(0);
     visited[0] = true;
-    
+
     while (!s.empty()) {
         int current = s.top();
         s.pop();
-        
         cout << vname[current] << " ";
-        
+
         for (int i = 0; i < n; i++) {
             if (matrix[current][i] != 0 && !visited[i]) {
                 s.push(i);
@@ -71,50 +70,54 @@ int main(){
     int n;
     cout << "Enter Number of Landmarks: ";
     cin >> n;
-    
+
     vector<vector<int>> matrix(n, vector<int>(n, 0));
+    vector<vector<int>> adjList(n); // Separate adjacency list for BFS
     vector<char> vname;
     char vn;
-    
+
     for(int i = 0; i < n; i++){
         cout << "\nEnter Name of Landmark " << i + 1 << ": ";
         cin >> vn;
         vname.push_back(vn);
     }
-    
+
     int weight;
-    for(int i = 0; i < matrix.size(); i++){
-        for(int j = 0; j < matrix[i].size(); j++){
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){ // Avoid duplicate edges & self-loops
             char inp;
-            cout << "\nIs there an Path between " << vname[i] << " to " << vname[j] << " (y/n): ";
+            cout << "\nIs there a path between " << vname[i] << " and " << vname[j] << " (y/n): ";
             cin >> inp;
             if(inp == 'y'){
-                cout << "\nEnter Distance of the Path: ";
+                cout << "Enter Distance of the Path: ";
                 cin >> weight;
                 matrix[i][j] = weight;
+                matrix[j][i] = weight; // Since graph is undirected
+
+                adjList[i].push_back(j); // Add to adjacency list
+                adjList[j].push_back(i);
             }
         }
     }
-    
-    cout << "Adjacency matrix representation: " << endl;
-    cout << "   ";
-    for(int i = 0; i < vname.size(); i++){
+
+    // Display adjacency matrix
+    cout << "\nAdjacency Matrix Representation: \n   ";
+    for(int i = 0; i < n; i++){
         cout << vname[i] << "  ";
     }
     cout << endl;
     
-    for(int i = 0; i < matrix.size(); i++){
+    for(int i = 0; i < n; i++){
         cout << vname[i] << "  ";
-        for(int j = 0; j < matrix[i].size(); j++){
+        for(int j = 0; j < n; j++){
             cout << matrix[i][j] << "  "; 
         }
         cout << endl;
     }
-    
-    displayAdjList(matrix, vname);
-    BFSTraversal(matrix, vname);
+
+    displayAdjList(adjList, vname);
+    BFSTraversal(adjList, vname);
     DFSTraversal(matrix, vname);
-    
+
     return 0;
 }
-
